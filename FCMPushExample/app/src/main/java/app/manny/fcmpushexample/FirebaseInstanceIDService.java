a86679ae504120e830312035fc5eabf92fb87b03
itzmemanny <znetz@naver.com>
2016-12-06 22:06:22 +0900

    first

---
+++ FCMPushExample/app/src/main/java/app/manny/fcmpushexample/FirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package app.manny.fcmpushexample;
+
+import android.app.Service;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by admin on 2016-12-06.
+ */
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "MyFirebaseIIDService";
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + token);
+
+
+
+    }
+}
